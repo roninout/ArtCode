c2ad182d461b4060d30e1996f3f1f8592ba837ed
roninout <roninorgua@gmail.com>
2017-01-13 16:14:17 +0200

    week 1 - task 4.2

---
+++ src/com/artcode/week1/task_4_7.java
@@ -0,0 +1,22 @@
+package com.artcode.week1;
+
+import java.util.Scanner;
+
+/**
+ * Created by Kovalchuk_A on 13.01.2017.
+ */
+
+// 4.7. Вводим два числа, если одно из них делиться на другое без остатка, то выводим тру и показываем результат деления (целую часть от деления  и остачу)
+//      в другом случае выводим false и показываем результат деления (целую часть от деления  и остачу)
+
+public class task_4_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите 1-е число:");
+        int numb1 = sc.nextInt();
+        System.out.println("Введите 2-е число:");
+        int numb2 = sc.nextInt();
+
+
+    }
+}
