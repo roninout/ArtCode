6525e7702f96307f298671ade6e7a3917ac84617
roninout <roninorgua@gmail.com>
2017-01-16 16:51:11 +0200

    week 1 - task 4.2

--- src/com/artcode/week1.additional/task_4.java
+++ src/com/artcode/week1.additional/task_4.java
@@ -1,5 +1,7 @@
 package com.artcode.week1.additional;
 
+import java.util.Scanner;
+
 /**
  * Created by Kovalchuk_A on 16.01.2017.
  */
@@ -9,4 +11,16 @@ package com.artcode.week1.additional;
 ду. Какая из них больше?
  */
 public class task_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите скорость в км/час:");
+        double a = sc.nextDouble();
+        System.out.println("Введите скорость в м/сек:");
+        double b = sc.nextDouble();
+
+        if (a > (36*b)/10)
+            System.out.println(a + " > " + b + " потому как " + b + " м/сек = "+ (36*b)/10+ " км/час");
+        else
+            System.out.println(a + " < " + b + " потому как " + b + " м/сек = "+ (36*b)/10+ " км/час");
+    }
 }
--- src/com/artcode/week1.additional/task_5.java
+++ src/com/artcode/week1.additional/task_5.java
@@ -1,5 +1,7 @@
 package com.artcode.week1.additional;
 
+import java.util.Scanner;
+
 /**
  * Created by Kovalchuk_A on 16.01.2017.
  */
@@ -10,4 +12,22 @@ package com.artcode.week1.additional;
  */
 
 public class task_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите 1-е число:");
+        int a = sc.nextInt();
+        System.out.println("Введите 2-е число:");
+        int b = sc.nextInt();
+        System.out.println("Введите 3-е число:");
+        int c = sc.nextInt();
+
+        if (a == b)
+            System.out.println("1-е = 2-у");
+        else {
+            if (a == c)
+                System.out.println("1-е = 3-у");
+            else
+                if (b == c) System.out.println("2-е = 3-у");
+        }
+    }
 }
