a69812680c15e914df1cd7bab480c39a05219aa5
roninout <roninorgua@gmail.com>
2017-01-16 14:44:19 +0200

    week 1 - task 4.2

--- src/com/artcode/week1.additional/task_3.java
+++ src/com/artcode/week1.additional/task_3.java
@@ -1,5 +1,7 @@
 package com.artcode.week1.additional;
 
+import java.util.Scanner;
+
 /**
  * Created by Kovalchuk_A on 16.01.2017.
  */
@@ -10,4 +12,21 @@ package com.artcode.week1.additional;
  */
 
 public class task_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите трехзначное число:");
+        int x = sc.nextInt();
+
+        int a = x%10;
+        int b = x/10%10;
+        int c = x/100%10;
+
+        if (a == b && a == c) System.out.println("Все цифры одинаковые");
+        else
+        {
+            if (a == b) System.out.println("1-е = 2-у");
+            if (a == c) System.out.println("1-е = 3-у");
+            if (b == c) System.out.println("2-е = 3-у");
+        }
+    }
 }
