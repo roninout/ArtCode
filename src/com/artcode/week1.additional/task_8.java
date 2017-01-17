ad3e2172b25c7f3cb28b8de7a433379182c3591e
roninout <roninorgua@gmail.com>
2017-01-17 08:35:11 +0200

    week 1 - task 4.2

--- src/com/artcode/week1.additional/task_8.java
+++ src/com/artcode/week1.additional/task_8.java
@@ -1,5 +1,7 @@
 package com.artcode.week1.additional;
 
+import java.util.Scanner;
+
 /**
  * Created by Kovalchuk_A on 16.01.2017.
  */
@@ -9,4 +11,14 @@ package com.artcode.week1.additional;
 тавляется, если сумма покупки превышает 1000 гривен.
  */
 public class task_8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите стоимость покупки:");
+        int a = sc.nextInt();
+
+        if (a > 1000)
+            System.out.println("Ваша скидка составляет " + (double)a/10 + " грн. Стоимость со скидкой " + ((double)a - (double)a/10) + " грн.");
+        else
+            System.out.println("Вам не предоставляется скидка, стоимость составляет " + a + " грн.");
+    }
 }
