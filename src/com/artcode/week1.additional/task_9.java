84b459a0768c67208adb0d57cd32d7373363d38a
roninout <roninorgua@gmail.com>
2017-01-17 10:05:32 +0200

    week 1 - task 4.2

--- src/com/artcode/week1.additional/task_9.java
+++ src/com/artcode/week1.additional/task_9.java
@@ -1,5 +1,7 @@
 package com.artcode.week1.additional;
 
+import java.util.Scanner;
+
 /**
  * Created by Kovalchuk_A on 16.01.2017.
  */
@@ -11,4 +13,23 @@ package com.artcode.week1.additional;
  */
 
 public class task_9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите свой рост в см:");
+        double height = sc.nextDouble();
+        System.out.println("Введите свой вес в кг:");
+        double weight = sc.nextDouble();
+
+        if (height <= 100) System.out.println("Не парься, ты еще ребенок, у тебя все идеально");
+        else {
+            if (weight == height - 100) System.out.println("У вас идеальный вес");
+            else {
+                if (weight > height - 100)
+                    System.out.println("Вам необходимо сбросить " + (weight - (height - 100)) + " кг.");
+                else
+                    System.out.println("Вам необходимо набрать " + ((height - 100) - weight) + " кг.");
+            }
+        }
+
+    }
 }
