bf5794780c63aa79dbbb33ab9203a2502e04364f
roninout <roninorgua@gmail.com>
2017-01-17 10:53:08 +0200

    week 1 - task 4.2

--- src/com/artcode/week1.additional/task_11.java
+++ src/com/artcode/week1.additional/task_11.java
@@ -1,5 +1,7 @@
 package com.artcode.week1.additional;
 
+import java.util.Scanner;
+
 /**
  * Created by Kovalchuk_A on 16.01.2017.
  */
@@ -11,4 +13,13 @@ package com.artcode.week1.additional;
 */
 
 public class task_11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество копеек:");
+        int coins = scanner.nextInt();
+
+        int rest = coins % 10;
+
+        System.out.println(coins + (rest == 0 || (rest > 4 && rest <= 9) ? " копеек" : rest == 1 ? " копейка" : " копейки"));
+    }
 }
